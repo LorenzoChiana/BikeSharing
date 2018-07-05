4772dcbb589211cbb176867a182cf29a17e0c366
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2018-07-05 12:50:31 +0200

    added navbar mobile version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,16 @@ import { LoginRegModule } from './login-reg/login-reg.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatToolbarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 const appRoutesMain: Routes = [
     { path: '', redirectTo: '/', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
@@ -17,10 +27,18 @@ const appRoutesMain: Routes = [
     HomeComponent,
   ],
   imports: [
-	BrowserModule,
-  LoginRegModule,
-  RouterModule.forRoot(appRoutesMain),
+    BrowserModule,
+    LoginRegModule,
+    RouterModule.forRoot(appRoutesMain),
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    FlexLayoutModule
   ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
