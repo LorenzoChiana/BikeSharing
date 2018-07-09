bc61ec14cb2cc1d577b6ff0ba44eb86bd1168970
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2018-07-09 17:44:27 +0200

    map on contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { } from '@types/googlemaps';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
@@ -6,14 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  lat: number = 44.1493031;
-  lng: number = 12.192423;
-
-  zoom: number = 12;
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+  	var lat: number = 44.139771;
+   	var lng: number = 12.243437;
+
+   	var markerCenter = new google.maps.LatLng(lat, lng);
+    var mapProp = {
+      center: new google.maps.LatLng(lat, lng),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
+      
+    var marker = new google.maps.Marker({
+        position: markerCenter
+    });
+    marker.setMap(this.map);
+    
   }
 
 }
