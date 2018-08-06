9933c19bf4a6e6bf54297982cbc02525491e084c
Lorenzo Chiana <lorenzo.chiana@studio.unibo.it>
2018-08-07 01:30:14 +0200

    Fixed bug contect

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,34 +8,13 @@ import { ViewChild } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-  @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
-  	var lat: number = 44.139771;
-   	var lng: number = 12.243437;
-
-   	var markerCenter = new google.maps.LatLng(lat, lng);
-    var mapProp = {
-      center: new google.maps.LatLng(lat, lng),
-      zoom: 15,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
-
-    var marker = new google.maps.Marker({
-        position: markerCenter
-    });
-    marker.setMap(this.map);
-
+    private latitudine: number = 44.139771;
+    private longitudine: number = 12.243437;
+    private zoom: number = 16;
   }
-
-  /*
-  initMap(): void {
-
-  }
-  */
 
 }
